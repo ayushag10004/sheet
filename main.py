33415c363973b39a56efff42ab0ba66afae698c9
ayushag10004 <ayushag10004@gmail.com>
2019-11-15 07:08:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from client import gc
+from datetime import date
+
+start_date = date(2019, 11, 15).toordinal()
 
 sheet = gc.open("Sheet").sheet1
+sheet.clear()
 
 users = {}
-users_count = 0
+users_count = 1
+sheet.update_cell(1, 1, "DATE")
 
 def add_user(name):
 	global users_count, users
@@ -11,10 +16,20 @@ def add_user(name):
 	users[name] = users_count
 	sheet.update_cell(1, users_count, name)
 
-sheet.clear()
+def mark_attendence(name, date):
+    global users, start_date
+    col_index = users[name]
+    curr_date = date.toordinal() 	
+    row_index = curr_date - start_date + 1
+    sheet.update_cell(row_index, 1, date.__str__() )
+    sheet.update_cell(row_index, col_index, "Done")
+
 add_user("Ayush")
 add_user("Sachin")
-
+mark_attendence("Ayush", date(2019, 11, 18) )
+mark_attendence("Sachin", date(2019, 11, 18) )
+mark_attendence("Sachin", date(2019, 11, 22) )
+mark_attendence("Ayush", date(2019, 11, 23) )
 
 
 # def mark_attendence(name, date):
